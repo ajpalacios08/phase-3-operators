d1b766d9c810ac6073331b91938d8db22e4e2a60
Allen <apalacios1994@gmail.com>
2021-05-18 13:52:39 -0500

    commit tree fca8354e6af420c8d435018421602fd08f4510da

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+    speed < 40 || speed > 60 ? true:false
 end
 
 
 
+
+
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true:false
 end
 	
 
